e4a512ed6a5f603e0c42882603d42d0407353ee6
szervoudakis <stezer95@gmail.com>
2025-09-18 12:12:45 +0300

    init project

---
+++ PaymentConsumerApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        // Queue for Push Notifications
+        channel.ExchangeDeclare("payment_events", ExchangeType.Fanout);
+
+        //auto generated bound que to payments events
+        var queueName = channel.QueueDeclare().QueueName;
+        channel.QueueBind(queue: queueName, exchange: "payment_events", routingKey: "");
+
+        Console.WriteLine("*] PaymentConsumer waiting for payment_request events...");
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+            Console.WriteLine($" [PaymentConsumer] Creating payment record: {message}");
+        };
+
+        channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+        
+        Console.ReadLine();
+    }
+}
---
+++ UserService/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace UserService.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
